750188b69f70419294c4a92586c12c7c2d27f7a4
Michael Mainguy <mike.mainguy@gmail.com>
2024-10-16 07:58:08 -0500

    updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "@mantine/core/styles.css";
-import {Button, Group, Input, MantineProvider, Textarea} from "@mantine/core";
+import {Button, Input, MantineProvider, Textarea, TextInput} from "@mantine/core";
 import {theme} from "./theme";
-import {useState} from "react";
+import React, {useState} from "react";
 import JsonSignature from "./jsonSignature.ts";
 
 const composer = new JsonSignature();
@@ -16,22 +16,20 @@ export default function App() {
     const [verifiedText, setVerifiedText] = useState('');
     const [publicKey, setPublicKey] = useState(key);
 
-    const sign = async() => {
-            try {
-                const json = JSON.parse(inputText);
-                const signed = await composer.sign(key, json);
-                setOutputText(JSON.stringify(signed));
-            } catch (e) {
-                setOutputText(e.message);
-            }
-
-
+    const sign = async () => {
+        try {
+            const json = JSON.parse(inputText);
+            const signed = await composer.sign(key, json);
+            setOutputText(JSON.stringify(signed));
+        } catch (e) {
+            setOutputText(e.message);
+        }
     }
-    const verify = async() => {
-            setVerifiedText('Working...');
-            const input = JSON.parse(outputText);
-            const verified = await composer.verify(input);
-            setVerifiedText(JSON.stringify(verified));
+    const verify = async () => {
+        setVerifiedText('Working...');
+        const input = JSON.parse(outputText);
+        const verified = await composer.verify(input);
+        setVerifiedText(JSON.stringify(verified));
     }
 
     const generate = async () => {
@@ -39,15 +37,20 @@ export default function App() {
     }
     return <MantineProvider theme={theme}>
         <Button key="generate" onClick={generate}>Generate Key Pair</Button>
-            <Input key="public" defaultValue={publicKey} label="Public Key"
-                   placeholder="Enter your private key"/>
+        <TextInput key="public" defaultValue={publicKey} label="Public Key"
+               placeholder="Enter your private key"/>
 
-        <Textarea label="Input" defaultValue={inputText} onChange={(e) => {setInputText(e.currentTarget.value)}} minRows={10} autosize={true} placeholder="Enter your message"/>
-        <Textarea label="Output" defaultValue={outputText} onChange={(e) => {setOutputText(e.currentTarget.value)}} minRows={10} autosize={true}
-                  placeholder="Enter your message"/>
+        <Textarea label="Input" defaultValue={inputText} onChange={(e) => {
+            setInputText(e.currentTarget.value)
+        }} minRows={5} autosize={true} placeholder="Enter your message"/>
         <Button onClick={sign}>Sign</Button>
+        <Textarea label="Output" defaultValue={outputText} onChange={(e) => {
+            setOutputText(e.currentTarget.value)
+        }} minRows={5} autosize={true}
+                  placeholder="Enter your message"/>
+
         <Button onClick={verify}>Verify</Button>
-        <Textarea label="Verified Output" defaultValue={verifiedText} minRows={10} autosize={true}
+        <Textarea label="Verified Output" defaultValue={verifiedText} minRows={5} autosize={true}
                   placeholder="Verification Result"/>
     </MantineProvider>;
 }
--- src/jsonSignature.ts
+++ src/jsonSignature.ts
@@ -1,21 +1,23 @@
 import * as jose from 'jose';
-import {GenerateKeyPairResult, JWK} from 'jose';
+import {GenerateKeyPairResult, JWK, KeyLike} from 'jose';
 import {v4 as uuidv4} from 'uuid';
-
-type KeySet = {
-    key: GenerateKeyPairResult,
-    alg: string
-}
+import stringify from "fast-json-stable-stringify";
+const iterations = 10000;
 export type SignedObject = {
     signature: string,
     protected: string | undefined,
     payload: object,
-    kid?: string,
-    jwk?: JWK
 }
+
+export type KeyPair = {
+    publicKey?: KeyLike,
+    privateKey?: KeyLike,
+    alg?: string
+}
+
 export type KeyStore = {
-    set: (key: string, value: KeySet) => void,
-    get: (key: string) => KeySet | undefined
+    set: (key: string, value: KeyPair) => void,
+    get: (key: string) => KeyPair | undefined
 }
 export default class JsonSignature {
     private readonly _keys: KeyStore;
@@ -24,53 +26,78 @@ export default class JsonSignature {
         if (keyStore) {
             this._keys = keyStore
         } else {
-            this._keys = new Map<string, KeySet>();
+            this._keys = new Map<string, KeyPair>();
         }
+    }
+    setPrivateKey(key: string, value: KeyLike) {
+        this._keys.set(key, {privateKey: value});
+    }
+    setPublicKey(key: string, value: KeyLike) {
+        this._keys.set(key, {publicKey: value});
     }
     public async getPublicKey(key: string): Promise<JWK> {
         const value = this._keys.get(key);
-        if (!value) {
+        if (!value?.publicKey) {
             throw new Error('Key not found');
         }
-        return await jose.exportJWK(value.key.publicKey);
+        return await jose.exportJWK(value.publicKey);
     }
 
     public async generateKeyPair(alg: string = 'PS256'): Promise<{ key: string, publicKey: JWK }> {
-        const value: GenerateKeyPairResult = await jose.generateKeyPair(alg, {extractable: false});
+        const value: GenerateKeyPairResult = await jose.generateKeyPair(alg, {extractable: true});
         const key = uuidv4();
-        this._keys.set(key, {key: value, alg: alg});
+        this._keys.set(key, {publicKey: value.publicKey, privateKey: value.privateKey, alg: alg});
         const pubKey = await jose.exportJWK(value.publicKey);
         return {key, publicKey: pubKey};
     }
 
     public async sign(key: string, obj: object): Promise<SignedObject> {
         const value = this._keys.get(key);
-        if (!value) {
+
+        if (!value?.privateKey) {
+            console.log( 'Key not found for ', key);
             throw new Error('Key not found');
         }
-        const input = JSON.stringify(obj);
+        let input = '';
+        let jws = {signature: '', protected: '', payload: ''};
+        const start = performance.now();
+        console.log('start: ', start);
+        for (let i = 0; i < iterations; i++) {
+            input = stringify(obj);
+            jws = await new jose.FlattenedSign(
+                new TextEncoder().encode(input))
+                .setProtectedHeader({alg: value?.alg, kid: key, b64: false, crit: ['b64']})
+                .sign(value.privateKey);
+        }
+        console.log('Time taken: ', performance.now() - start);
+        console.log('Time taken: ', (performance.now() - start)/iterations);
 
-        const jws = await new jose.FlattenedSign(
-            new TextEncoder().encode(input))
-            .setProtectedHeader({alg: "PS256", kid: key, b64: false, crit: ['b64']})
-            .sign(value.key.privateKey);
         return {
             signature: jws.signature,
             protected: jws.protected,
-            payload: obj,
-            kid: key,
-            jwk: await jose.exportJWK(value.key.publicKey)
+            payload: JSON.parse(input),
         }
     }
-
     public async verify(signed: SignedObject, key?: string): Promise<object> {
+        let v = {};
+        const start = performance.now();
+        console.log('start: ', start);
+        for (let i = 0; i < iterations; i++) {
+            v = await this._verify(signed, key);
+        }
+        console.log('Time taken: ', performance.now() - start);
+        console.log('Time taken: ', (performance.now() - start)/iterations);
+        return v;
+
+    }
+    private async _verify(signed: SignedObject, key?: string): Promise<object> {
         let kid = key;
         try {
-            const header = await jose.decodeProtectedHeader({protected: signed.protected, signature: signed.signature});
+            const header = jose.decodeProtectedHeader({protected: signed.protected, signature: signed.signature});
             if (header.kid) {
                 kid = header.kid;
             }
-            console.log(header);
+
         } catch (e) {
             return {error: e.message};
         }
@@ -78,18 +105,22 @@ export default class JsonSignature {
             return {error: 'kid not found'};
         }
         const value = this._keys.get(kid);
-        if (!value?.key?.publicKey) {
+        if (!value?.publicKey) {
             return {error: `Key ${kid} not found`};
         }
-        const rsaPublicKey = value.key.publicKey;
+        const rsaPublicKey = value.publicKey;
+        let options = {};
+        if (value.alg) {
+            options = {algorithms: [value.alg]}
+        };
         try {
             const {
                 payload
             } = await jose.flattenedVerify(
                 { signature: signed.signature,
                     protected: signed.protected,
-                    payload: JSON.stringify(signed.payload)}
-                , rsaPublicKey, {algorithms: ['PS256']});
+                    payload: stringify(signed.payload)}
+                , rsaPublicKey, options);
             return JSON.parse(new TextDecoder().decode(payload));
         } catch (e) {
             return {error: e.message};
